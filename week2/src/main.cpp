8b1c2b222b7217853aa83662e1d1f11f64f1ba88
ThomasGrafNumerics <thomas.graf.numerics@gmail.com>
2023-03-14 16:40:42 +0100

    rook attacks

--- week2/src/main.cpp
+++ week2/src/main.cpp
@@ -7,12 +7,23 @@ int main(void)
 {
 	Pieces_Attacks attacks;
 	Bitboard board;
+	Bitboard blockers;
+	blockers.set_bit(D4);
+	blockers.set_bit(D2);
+	blockers.set_bit(A6);
+	blockers.set_bit(F6);
+	blockers.set_bit(B6);
+	blockers.set_bit(D7);
+	blockers.set_bit(H1);
 
-	for (unsigned square = 0; square < 64; ++square)
-	{
-		board = attacks.pawn_attacks_used_to_precompute_attack_tables_later(WHITE, square);
-		board.print_bitboard();
-	}
+	board = attacks.rook_attacks_used_to_precompute_attack_tables_later(blockers, H1);
+	board.print_bitboard();
+
+	// for (unsigned square = 0; square < 64; ++square)
+	// {
+	// 	board = attacks.knight_attacks_used_to_precompute_attack_tables_later(square);
+	// 	board.print_bitboard();
+	// }
 
 	return 0;
 }
--- week2/src/pieces_attacks.cpp
+++ week2/src/pieces_attacks.cpp
@@ -1,4 +1,5 @@
 #include "pieces_attacks.h"
+
 Bitboard Pieces_Attacks::pawn_attacks_used_to_precompute_attack_tables_later(const bool side, const unsigned int square) const
 {
 	Bitboard attack(0), temp(0);
@@ -18,13 +19,23 @@ Bitboard Pieces_Attacks::pawn_attacks_used_to_precompute_attack_tables_later(con
 	return attack;
 }
 
-// Bitboard Pieces_Attacks::knight_attacks_used_to_precompute_attack_tables_later(const unsigned int square) const
-// {
-// 	Bitboard attack(0), temp(0);
-// 	temp.set_bit(square);
+Bitboard Pieces_Attacks::knight_attacks_used_to_precompute_attack_tables_later(const unsigned int square) const
+{
+	Bitboard attack(0), temp(0);
+	temp.set_bit(square);
 
-// 	return attack;
-// }
+	attack |= ((temp >> 17) & NOT_H_FILE_MASK );
+	attack |= ((temp >> 15) & NOT_A_FILE_MASK );
+	attack |= ((temp >> 10) & NOT_GH_FILE_MASK);
+	attack |= ((temp >>  6) & NOT_AB_FILE_MASK);
+
+	attack |= ((temp << 17) & NOT_A_FILE_MASK );
+	attack |= ((temp << 15) & NOT_H_FILE_MASK );
+	attack |= ((temp << 10) & NOT_AB_FILE_MASK);
+	attack |= ((temp <<  6) & NOT_GH_FILE_MASK);
+
+	return attack;
+}
 
 // Bitboard Pieces_Attacks::king_attacks_used_to_precompute_attack_tables_later(const unsigned int square) const
 // {
@@ -34,13 +45,46 @@ Bitboard Pieces_Attacks::pawn_attacks_used_to_precompute_attack_tables_later(con
 // 	return attack;
 // }
 
-// Bitboard Pieces_Attacks::rook_attacks_used_to_precompute_attack_tables_later(const Bitboard blockers, const unsigned int square) const
-// {
-// 	Bitboard attack(0);
+Bitboard Pieces_Attacks::rook_attacks_used_to_precompute_attack_tables_later(const Bitboard blockers, const unsigned int square) const
+{
+	Bitboard attack(0);
+	const int rank = square / 8;
+	const int file = square % 8;
 
+	// ray to South
+	for (int r = rank + 1, f = file; r <= 7; ++r)
+	{
+		Bitboard bitboard = 1_int64 << (r * 8 + f);
+		attack |= bitboard;
+		if (bitboard & blockers) break;
+	}
 
-// 	return attack;
-// }
+	// ray to North
+	for (int r = rank - 1, f = file; r >= 0; --r)
+	{
+		Bitboard bitboard = 1_int64 << (r * 8 + f);
+		attack |= bitboard;
+		if (bitboard & blockers) break;
+	}
+
+	// ray to East
+	for (int r = rank, f = file + 1; f <= 7; ++f)
+	{
+		Bitboard bitboard = 1_int64 << (r * 8 + f);
+		attack |= bitboard;
+		if (bitboard & blockers) break;
+	}
+	
+	// ray to West
+	for (int r = rank, f = file - 1; f >= 0; --f)
+	{
+		Bitboard bitboard = 1_int64 << (r * 8 + f);
+		attack |= bitboard;
+		if (bitboard & blockers) break;
+	}
+
+	return attack;
+}
 
 // Bitboard Pieces_Attacks::bishop_attacks_used_to_precompute_attack_tables_later(const Bitboard blockers, const unsigned int square) const
 // {
