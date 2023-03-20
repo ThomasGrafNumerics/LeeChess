bbd8a326f863e7a8b2a4b7f81c01611770d38e3f
ThomasGrafNumerics <thomas.graf.numerics@gmail.com>
2023-03-20 22:36:24 +0100

    Eduard's and Mark's king and bishop attacks

---
+++ week3/build/src/get_magic_number_candidate.cpp.d
@@ -0,0 +1,2 @@
+build/./src/get_magic_number_candidate.cpp.o: \
+ src/get_magic_number_candidate.cpp
---
+++ week3/src/get_magic_number_candidate.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "types.h"
+
+class Get_Magic_Number_Candidate
+{
+	public:
+		
+
+
+	private:
+		
+};
